fccd1be35ad8108cf1e353de2239a04b2678287b
‘Jan-Jaap’ <janjaap.vandevelde@gmail.com>
2022-09-29 23:46:20 +0200

    st_laptime_optimize

--- st_laptime_optimize.py
+++ st_laptime_optimize.py
@@ -1,16 +1,26 @@
-
 import pandas as pd
 import json, os
 
 from track_sim.sim import Car, Track
 from utilities.timer import Timer
 
-PATH_RESULTS_ = './simulated/'
-PATH_TRACKS = './tracks/'
-PATH_CARS = './cars/'
+PATH_RESULTS_   = './simulated/'
+PATH_TRACKS     = './tracks/'
+PATH_CARS       = './cars/'
 
 NAME_CAR = "Peugeot_205RFS"
-NAME_TRACK = "202209022_Circuit_Meppen"
+NAME_TRACK = "20191211_Bilsterberg"
+
+OUTPUT_COLUMNS_NAMES = dict(
+    distance = 'Distance (m)',
+    line_x = 'line_x',
+    line_y='line_y',
+    speed='Speed (m/s)',
+    a_lon='Longitudinal acceleration (m/s2)',
+    a_lat='Lateral acceleration (m/s2)',
+    race_line_position='Race line',
+    time='Timestamp',
+)
 
 filename_car_properties = f"{PATH_CARS}{NAME_CAR}.json"
 filename_track = f"{PATH_TRACKS}{NAME_TRACK}.csv"
@@ -19,17 +29,10 @@ filename_results = f'{PATH_RESULTS_}{NAME_CAR}_{NAME_TRACK}_simulated.csv'
 def laptime_str(seconds):
     return "{:02.0f}:{:06.03f}".format(seconds%3600//60, seconds%60)
 
-def return_dataframe(df, results):
-    df['Distance (m)']=results.distance
-    df['line_x']=results.line_x
-    df['line_y']=results.line_y
-    df['Speed (m/s)'] = results.speed
-    df['Speed (km/h)'] = results.speed * 3.6
-    df['Longitudinal acceleration (m/s2)'] = results.a_lon
-    df['Lateral acceleration (m/s2)'] = results.a_lat
-    df['Race line'] = results.race_line_position
-    df['Timestamp'] = results.time
-    return df
+def save_results(df, results):
+    results = results.rename(columns = OUTPUT_COLUMNS_NAMES)
+    df.drop(columns=results.columns, errors='ignore').join(results).to_csv(filename_results, index = None, header=True)
+
 
 #%% main scripts
 def main():
@@ -84,7 +87,7 @@ def main():
                 results = track.race(race_car, best_known_raceline, verbose=True)
 
                 
-                return_dataframe(df_track, results).to_csv(filename_results, index = None, header=True)
+                save_results(df_track, results)#.to_csv(filename_results, index = None, header=True)
                 print(f'intermediate results saved to {filename_results=}')
                 timer2.reset()
                 
@@ -98,8 +101,9 @@ def main():
         os.makedirs(PATH_RESULTS_)
 
     results = track.race(race_car, best_known_raceline, verbose=True)
-    return_dataframe(df_track, results).to_csv(filename_results, index = None, header=True)
-    
+    # return_dataframe(df_track, results).to_csv(filename_results, index = None, header=True)
+    save_results(df_track,  results)#.rename(columns = OUTPUT_COLUMNS_NAMES)).to_csv(filename_results, index = None, header=True)
+
     print(f'{race_car.name} - Simulated laptime = {laptime_str(laptime)}')
 
 
