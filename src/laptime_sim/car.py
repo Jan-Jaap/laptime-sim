6d83e303b47966e14d13f97ed642817ab37fd237
JanJaapvandeVelde <jj.vd>
2023-11-10 09:46:53 +0100

    toml car description

--- src/laptime_sim/car.py
+++ src/laptime_sim/car.py
@@ -2,9 +2,23 @@ from dataclasses import dataclass
 import numpy as np
 import json
 
+import toml
+
 
 @dataclass
 class Car:
+    """
+        Args:
+        mass:           Mass of car in kg
+        P_engine:       Engine power in HP
+        acc_limit:      grip limit in longitudinal direction under acceleration
+        dec_limit:      grip limit in longitudinal direction under braking
+        acc_grip_max:   float
+        c_drag:         float
+        c_roll:         float
+        name:           str = None
+        trail_braking:  float = 70
+    """
     mass:           float
     P_engine:       float
     acc_limit:      float
@@ -16,13 +30,22 @@ class Car:
     trail_braking:  float = 70
 
     @classmethod
-    def from_file(cls, filename):
-
+    def from_json(cls, filename):
+        '''load car parameters from JSON file'''
         with open(filename, 'r') as fp:
             return cls(**json.load(fp))
 
+    @classmethod
+    def from_toml(cls, filename):
+        '''load car parameters from TOML file'''
+        return cls(**toml.load(filename))
+
     def get_max_acc(self, v, acc_lat):
-        '''maximum possible acceleration (flooring it)'''
+        '''maximum possible acceleration (flooring it)
+        args:
+            v:          velocity in m/s
+            acc_lat:    lateral acceleration in m/s²
+        '''
         # grip circle (no downforce accounted for)
         acc_lon_max = self.acc_limit / self.acc_grip_max * (self.acc_grip_max**2 - acc_lat**2)**0.5
         # max lateral accceleration due to engine
@@ -39,8 +62,10 @@ class Car:
         return acc_lon
 
     def force_engine(self, v):
+        '''return available engine force at given velocity'''
         P_engine = self.P_engine / 1.3410 * 1000  # from hp to Watt
         return P_engine / v   # tractive force (limited by engine power)
 
     def get_gear(self, v):
-        return v*0
+        '''not implemented'''
+        pass
