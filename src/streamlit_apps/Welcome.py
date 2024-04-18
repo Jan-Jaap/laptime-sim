5dc7e8029f14b908479d3f9018f9836f5fedce9d
JanJaapvandeVelde <jj.vd>
2024-04-18 17:26:08 +0200

    update dependancies

--- src/streamlit_apps/Welcome.py
+++ src/streamlit_apps/Welcome.py
@@ -12,10 +12,19 @@ def main() -> None:
     st.set_page_config('Laptime Simulator')
     st.markdown(
         '''
-            **Laptime Simulator\n
+            **Laptime Simulator
+            \n
             by: Jan-Jaap van de Velde
 
             https://github.com/Jan-Jaap/laptime-sim
+
+            I'm no math wizard.
+            Optimization of laptimes is done by bruteforce trying different lines and using the fastest line found.
+            By trying thousands of different options, we can come close (enough) to the optimum raceline.
+            Some would call this AI...\n 
+            Please use the menu options in the sidebar\n
+            If the results viewer doesn't work, maybe create some results by running optimizations first :-)
+
         '''
     )
 
